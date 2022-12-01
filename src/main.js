978df22e2f9c591a5ad934fa68f22cfb03956a3c
doooby <zelazk.o@email.cz>
2022-12-01 16:06:00 +0100

    added bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import './assets/main.css'
+import "./assets/main.css";
+import "bootstrap/scss/bootstrap.scss";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
